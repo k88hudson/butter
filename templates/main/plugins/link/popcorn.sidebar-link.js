ae4febc45c481d0bb9d34b02df52776abb1c7ed5
Kate Hudson <katehudsondesign@gmail.com>
2012-06-01 13:54:21 -0400

    added fixes for links plugin

--- templates/main/plugins/link/popcorn.sidebar-link.js
+++ templates/main/plugins/link/popcorn.sidebar-link.js
@@ -1,92 +1,10 @@
-// PLUGIN: Text
+// PLUGIN: Link
 
 (function ( Popcorn ) {
 
   /**
-   * Text Popcorn plug-in
-   *
-   * Places text in an element on the page.  Plugin options include:
-   * Options parameter will need a start, end.
-   *   Start: Is the time that you want this plug-in to execute
-   *   End: Is the time that you want this plug-in to stop executing
-   *   Text: Is the text that you want to appear in the target
-   *   Escape: {true|false} Whether to escape the text (e.g., html strings)
-   *   Multiline: {true|false} Whether newlines should be turned into <br>s
-   *   Target: Is the ID of the element where the text should be placed. An empty target
-   *           will be placed on top of the media element
-   *
-   * @param {Object} options
-   *
-   * Example:
-   *  var p = Popcorn('#video')
-   *
-   *    // Simple text
-   *    .text({
-   *      start: 5, // seconds
-   *      end: 15, // seconds
-   *      text: 'This video made exclusively for drumbeat.org',
-   *      target: 'textdiv'
-   *     })
-   *
-   *    // HTML text, rendered as HTML
-   *    .text({
-   *      start: 15, // seconds
-   *      end: 20, // seconds
-   *      text: '<p>This video made <em>exclusively</em> for drumbeat.org</p>',
-   *      target: 'textdiv'
-   *    })
-   *
-   *    // HTML text, escaped and rendered as plain text
-   *    .text({
-   *      start: 20, // seconds
-   *      end: 25, // seconds
-   *      text: 'This is an HTML p element: <p>paragraph</p>',
-   *      escape: true,
-   *      target: 'textdiv'
-   *    })
-   *
-   *    // Multi-Line HTML text, escaped and rendered as plain text
-   *    .text({
-   *      start: 25, // seconds
-   *      end: 30, // seconds
-   *      text: 'This is an HTML p element: <p>paragraph</p>\nThis is an HTML b element: <b>bold</b>',
-   *      escape: true,
-   *      multiline: true,
-   *      target: 'textdiv'
-   *    });
-   *
-   *    // Subtitle text
-   *    .text({
-   *      start: 30, // seconds
-   *      end: 40, // seconds
-   *      text: 'This will be overlayed on the video',
-   *     })
-   **/
-
-  /**
-   * HTML escape code from mustache.js, used under MIT Licence
-   * https://github.com/janl/mustache.js/blob/master/mustache.js
-   **/
-  var escapeMap = {
-    "&": "&amp;",
-    "<": "&lt;",
-    ">": "&gt;",
-    '"': '&quot;',
-    "'": '&#39;'
-  };
-
-  function escapeHTML( string, multiline ) {
-    return String( string ).replace( /&(?!\w+;)|[<>"']/g, function ( s ) {
-      return escapeMap[ s ] || s;
-    });
-  }
-
-  function newlineToBreak( string ) {
-    // Deal with both \r\n and \n
-    return string.replace( /\r?\n/gm, "<br>" );
-  }
-
-
+   * Links Popcorn plug-in
+  */
   Popcorn.plugin( "sidebar-link", {
 
     manifest: {
@@ -119,6 +37,12 @@
           type: "number",
           label: "Timecode for link (in s)"
         },
+        style: {
+          elem: "input",
+          type: "text",
+          label: "styles",
+          "default": "btn btn-full-width btn-red"
+        }
         target: {
           elem: "select",
           options: ["now-playing-links"],
@@ -129,11 +53,12 @@
 
     _setup: function( options ) {
 
-      var target,
+      var _popcorn = this,
+          target,
           text,
           container = options._container = document.createElement( "a" );
 
-      container.style.display = "none  ";
+      //container.style.display = "none ";
 
       target = document.getElementById( options.target );
 
@@ -141,32 +66,25 @@
       options._target = target;
       text = options.text;
 
-      container.href = "#" + options.timecode || "";
+      container.setAttribute( "for", "#" + options.timecode || "" );
       container.innerHTML = text || "";
-      container.className = "btn btn-full-width btn-red";
+      container.className = options.style;
 
       target.appendChild( container );
 
+      container.addEventListener("click", function(e){
+        e.preventDefault();
+        var link = container.getAttribute("for").replace("#", "");
+        _popcorn.currentTime( link ).play();
+      },false);
+
     },
 
-    /**
-     * @member text
-     * The start function will be executed when the currentTime
-     * of the video  reaches the start time provided by the
-     * options variable
-     */
     start: function( event, options ) {
-      options._container.style.display = "block";
+      //options._container.style.display = "block";
     },
-
-    /**
-     * @member text
-     * The end function will be executed when the currentTime
-     * of the video  reaches the end time provided by the
-     * options variable
-     */
     end: function( event, options ) {
-      options._container.style.display = "none";
+      //options._container.style.display = "none";
     },
 
     _teardown: function( options ) {
